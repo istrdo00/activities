4ec1bc0625c9df01a7c85ee452ad8dcb58e9f81f
istrdo00 <106935491+istrdo00@users.noreply.github.com>
2022-06-06 02:33:48 +0300

    Update types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,3 @@
 // secrets: wrangler secret put <name>
-declare const publicKey: string
+declare const publicKey: 90189d16b834416629051707c1508a85b776994a6b2495e96e0d4387a25a478c
 declare const token: string
